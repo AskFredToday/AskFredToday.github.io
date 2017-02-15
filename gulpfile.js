08a00bc4707016d89f7a1f6afc33f1cb7672cb1a
Littlelies <frederic.minot@gmail.com>
2017-02-15 12:41:18 +0100

    Added support for external CSS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp'),
     jsugly = require('gulp-uglify'),
     riot = require('gulp-riot'),
+    minifyCss = require('gulp-minify-css'),
     rename = require('gulp-rename');
 
 gulp.task('default', function() {
   var opts = {
     conditionals: true
   };
+  gulp.src(['*/*.css', '!*/*.min.css'])
+    .pipe(minifyCss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./'));
+
   return gulp.src(['./*/*.tag.html'])
     .pipe(riot({
       compact: true
