cfbb6b1d36350f61ae7d3fa18a7fa0fdad85b934
Littlelies <frederic.minot@gmail.com>
2017-02-24 11:42:57 +0100

    Some bugfix, darker labels

--- af-select/af-select.tag.js
+++ af-select/af-select.tag.js
@@ -1,4 +1,4 @@
-riot.tag2('af-select', '<span class="input isFilled" onclick="{onFocus}">{currentTitle}</span><i class="material-icons" onclick="{onFocus}">keyboard_arrow_down</i><label onclick="{onFocus}">{opts.label}</label><div class="options"><ul><li each="{option in opts.options}" onclick="{selectOption}" class="{current : (currentTitle == option.title)}">{option.title}</li></ul></div><div class="overlay" onclick="{closeSelect}"></div>', '', '', function(opts) {
+riot.tag2('af-select', '<div class="input isFilled" onclick="{onFocus}">{currentTitle}</div><i class="material-icons" onclick="{onFocus}">keyboard_arrow_down</i><label onclick="{onFocus}">{opts.label}</label><div class="options"><ul><li each="{option in opts.options}" onclick="{selectOption}" class="{current : (currentTitle == option.title)}">{option.title}</li></ul></div><div class="overlay" onclick="{closeSelect}"></div>', '', '', function(opts) {
         var self = this;
         self.currentTitle = self.opts.initvalue.title;
 
