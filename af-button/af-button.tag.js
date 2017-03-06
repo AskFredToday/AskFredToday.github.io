dfd7506df595dff02b7c5a9238c14435981905c6
Littlelies <frederic.minot@gmail.com>
2017-03-06 12:25:10 +0100

    Added disabled support for button

--- af-button/af-button.tag.js
+++ af-button/af-button.tag.js
@@ -1,11 +1,21 @@
-riot.tag2('af-button', '<div onclick="{onClick}" class="af-button">{buttontext}</div>', '', '', function(opts) {
+riot.tag2('af-button', '<div onclick="{onClick}" class="{disabled : isdisabled}">{buttontext}</div>', '', '', function(opts) {
     var self = this;
     self.buttontext = opts.buttontext;
+    self.isdisabled = opts.isdisabled || false;
+
     this.onClick = function() {
-        self.opts.bus && self.opts.bus.trigger('click', self);
+        !self.isdisabled && self.opts.bus && self.opts.bus.trigger('click', self);
     };
     opts.bus && opts.bus.on('updatebuttontext', function(val) {
         self.buttontext = val;
+        self.update();
+    });
+    opts.bus && opts.bus.on('enable', function(val) {
+        self.isdisabled = false;
+        self.update();
+    });
+    opts.bus && opts.bus.on('disable', function(val) {
+        self.isdisabled = true;
         self.update();
     });
 });
